a4f24bf0d4bdfde4057a489cf8b8e0ec7a5dd185
fanetech <louguefranck.20@gmail.com>
2022-05-30 20:46:41 +0000

    add to file upload( logo and banniere picture) fontionality

---
+++ src/Service/UploaderFileService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploaderFileService
+{
+	public function uploader(UploadedFile $file, string $dossier, string $id)
+	{
+		$fileName = $id . "." . time() . ".jpg";
+		$file->move($dossier, $fileName);
+		return $fileName;
+	}
+}
